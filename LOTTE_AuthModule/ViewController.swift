6ce299275cf7bfe0338667fba7e6751b6c018f37
Kaeul Han <bc_automneh@naver.com>
2016-01-22 13:52:32 +0900

    Initial Commit

---
+++ LOTTE_AuthModule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LOTTE_AuthModule
+//
+//  Created by Kaeul Han on 2016. 1. 22..
+//  Copyright © 2016년 Jay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
